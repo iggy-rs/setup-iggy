5267aab9cdf7c621177f25526eb6c7928124279e
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-24 18:39:18 +0100

    chore: trigger CI

--- src/download-binary.ts
+++ src/download-binary.ts
@@ -1,12 +1,8 @@
-import * as core from "@actions/core";
 import tc from "@actions/tool-cache";
 
 export async function downloadFile(version: string): Promise<string> {
   const binaryUrl = `https://github.com/iggy-rs/iggy/releases/download/server-${version}/Linux-x86_64.zip`;
-  core.info(`Downloading from ${binaryUrl}`);
-
   const pathToTarball = await tc.downloadTool(binaryUrl);
   const pathToCLI = await tc.extractTar(pathToTarball);
-
   return pathToCLI;
 }
