0850630ce390dfd9f0ae12ede4f2bed405d2f8b9
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-24 18:34:49 +0100

    feat: push v1 of the Github action

---
+++ src/binary.ts
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+
+import { downloadFile } from "./download-binary";
+
+export async function setupBinary(version: string) {
+  core.info(`Installing iggy binary (${version})...`);
+
+  const binaryPath = await downloadFile(version);
+  core.info(`Binary path is: ${binaryPath}`);
+
+  core.addPath(binaryPath);
+  core.info(`iggy:${version} added to path`);
+
+  core.setOutput("version", version);
+}
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import * as core from "@actions/core";
+
+import { setupBinary } from "./binary";
+
+const DEFAULT_VERSION = "0.4.72";
+
+async function main() {
+  let version = core.getInput("version", { required: false });
+  if (version == null || version == "") {
+    version = DEFAULT_VERSION;
+  }
+
+  try {
+    core.info(`Installing iggy:${version} and adding it to GitHub Actions Path`);
+
+    await setupBinary(version);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  }
+}
+
+main();
