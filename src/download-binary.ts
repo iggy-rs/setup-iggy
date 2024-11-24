df61027525e6099f1388644477d67a791968ca1f
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-24 18:56:07 +0100

    fix: bad function used

--- src/download-binary.ts
+++ src/download-binary.ts
@@ -1,11 +1,11 @@
 import { info } from "@actions/core";
-import { downloadTool, extractTar } from "@actions/tool-cache";
+import { downloadTool, extractZip } from "@actions/tool-cache";
 
 export async function downloadFile(version: string): Promise<string> {
   const binaryUrl = `https://github.com/iggy-rs/iggy/releases/download/server-${version}/Linux-x86_64.zip`;
   info(`Binary downloaded from ${binaryUrl}`);
 
   const pathToTarball = await downloadTool(binaryUrl);
-  const pathToCLI = await extractTar(pathToTarball);
+  const pathToCLI = await extractZip(pathToTarball);
   return pathToCLI;
 }
