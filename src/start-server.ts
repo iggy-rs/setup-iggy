de6941719f88975798598fa7444902d5281fd821
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-27 19:48:14 +0100

    feat: ignore stdio

--- src/start-server.ts
+++ src/start-server.ts
@@ -3,8 +3,8 @@ import * as core from "@actions/core";
 import { spawn } from "child_process";
 
 export function startIggyServer() {
-  const iggy = spawn("iggy-server", [], { detached: true });
+  const iggy = spawn("iggy-server", [], { detached: true, stdio: "ignore" });
   iggy.unref();
 
-  core.info("Starting started!");
+  core.info("Server started!");
 }
