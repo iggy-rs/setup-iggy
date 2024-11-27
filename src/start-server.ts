e5aa693f726261e646e5c6210fd9c5cf9f37e3b3
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-27 19:34:27 +0100

    ci: use binary name

--- src/start-server.ts
+++ src/start-server.ts
@@ -1,7 +1,7 @@
 import { spawn } from "child_process";
 
 export async function startIggyServer() {
-  const iggy = spawn("/tmp/d7db3572-78c0-499b-a881-6726ea12f684/all_artifacts/Linux-x86_64", ["1"]);
+  const iggy = spawn("iggy-server");
 
   iggy.stdout.on("data", data => {
     console.log(`stdout: ${data}`);
