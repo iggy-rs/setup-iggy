1a4009a724127344474a5852ebc1b9a845ae464f
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-27 19:43:40 +0100

    feat: use unref

--- src/start-server.ts
+++ src/start-server.ts
@@ -2,16 +2,5 @@ import { spawn } from "child_process";
 
 export async function startIggyServer() {
   const iggy = spawn("iggy-server", [], { detached: true });
-
-  iggy.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
-  });
-
-  iggy.stderr.on("data", data => {
-    console.error(`stderr: ${data}`);
-  });
-
-  iggy.on("close", code => {
-    console.log(`child process exited with code ${code}`);
-  });
+  iggy.unref();
 }
