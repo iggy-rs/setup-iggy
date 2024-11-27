f9e64ae176f4cf7439609fd3c23ab7bd1a7f70b0
Tchoupinax <corentinfiloche@hotmail.fr>
2024-11-27 19:42:56 +0100

    feat: detach process

--- src/start-server.ts
+++ src/start-server.ts
@@ -1,7 +1,7 @@
 import { spawn } from "child_process";
 
 export async function startIggyServer() {
-  const iggy = spawn("iggy-server");
+  const iggy = spawn("iggy-server", [], { detached: true });
 
   iggy.stdout.on("data", data => {
     console.log(`stdout: ${data}`);
