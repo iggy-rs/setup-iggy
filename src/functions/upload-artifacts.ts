530a5f653e56972eef088cce7b60b6cf9adca979
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-16 18:26:36 +0100

    trigger ci

--- src/functions/upload-artifacts.ts
+++ src/functions/upload-artifacts.ts
@@ -3,14 +3,15 @@ import * as core from "@actions/core";
 import fs from "node:fs";
 
 export async function uploadLogs() {
-  const filename = "/tmp/setup-iggy/logs/*";
-
   const files = fs.readdirSync("/tmp/setup-iggy/logs");
-  console.log(files);
 
-  const { id, size } = await artifact.uploadArtifact("logs", [filename], process.cwd(), {
-    retentionDays: 10,
-  });
+  await Promise.allSettled(
+    files.map(async filename => {
+      const { id, size } = await artifact.uploadArtifact("logs", [filename], process.cwd(), {
+        retentionDays: 10,
+      });
 
-  core.info(`Created artifact with id: ${id} (bytes: ${size}`);
+      core.info(`Created artifact with id: ${id} (bytes: ${size}`);
+    }),
+  );
 }
