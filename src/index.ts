42a288ea62972733268c7ffe990e316c97f45a15
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-16 18:28:14 +0100

    trigger ci

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as core from "@actions/core";
 
 import { setupBinary } from "./binary";
-import { uploadLogs } from "./functions/upload-artifacts";
 import { startIggyServer } from "./start-server";
 
 const DEFAULT_VERSION = "latest";
@@ -22,16 +21,10 @@ async function main() {
       core.info("Starting server...");
       startIggyServer();
     }
-
-    await new Promise(r => setTimeout(r, 1000));
-
-    throw new Error();
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
     }
-
-    uploadLogs();
   }
 }
 
