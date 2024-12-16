91718c0c31a9b7e0eeadee0fd44c19fcf57693a0
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-16 18:24:11 +0100

    trigger ci

--- src/functions/upload-artifacts.ts
+++ src/functions/upload-artifacts.ts
@@ -1,8 +1,13 @@
 import artifact from "@actions/artifact";
 import * as core from "@actions/core";
+import fs from "node:fs";
 
 export async function uploadLogs() {
   const filename = "/tmp/setup-iggy/logs/*";
+
+  const files = fs.readdirSync("/tmp/setup-iggy/logs");
+  console.log(files);
+
   const { id, size } = await artifact.uploadArtifact("logs", [filename], process.cwd(), {
     retentionDays: 10,
   });
