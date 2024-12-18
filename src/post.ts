1ab303bc1a221020ee47a59b97377793c38766b2
Tchoupinax <corentinfiloche@hotmail.fr>
2024-12-18 17:25:15 +0100

    Trigger CI

--- src/post.ts
+++ src/post.ts
@@ -1,6 +1,10 @@
+import * as core from "@actions/core";
+
 import { uploadLogs } from "./functions/upload-artifacts";
 
 async function main() {
+  core.info("Post processing");
+
   await uploadLogs();
 }
 
